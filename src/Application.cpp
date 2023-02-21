bc2f198e0eb4e4d49514503fb71b805bc4b2df79
Alec Cleofe <aleccleofe@gmail.com>
2023-02-21 03:21:26 -0600

    Initialized GLEW

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>;
 
 int main(void)
 {
@@ -9,15 +11,17 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    std::cout << (glewInit() == GLEW_OK);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -27,7 +31,7 @@ int main(void)
 
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.0f, 1.0f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
 
